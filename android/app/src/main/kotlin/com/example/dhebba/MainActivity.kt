748e31b91b1de3e9c5233347e77c721095147d0c
Blaze <kozusrizann@gmail.com>
2024-06-02 15:40:24 +0545

    Clean up ignored files

--- android/app/src/main/kotlin/com/example/dhebba/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dhebba
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/app/controllers/controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:nylo_framework/nylo_framework.dart';
-
-///Controller for the Nylo
-/// See more on controllers here - https://nylo.dev/docs/5.20.0/controllers
-class Controller extends NyController {
-  Controller();
-}
